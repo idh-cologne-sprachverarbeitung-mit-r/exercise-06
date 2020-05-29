eef258e239b3455ea7863168d332c0aa6663c820
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-05-29 13:08:25 +0200

    test trunk

--- test.R
+++ test.R
@@ -2,3 +2,22 @@ library(testthat)
 
 source("R/exercise.R")
 
+test_that("a) yields the correct result", {
+	
+})
+
+test_that("b) yields the correct result", {
+	
+})
+
+test_that("c) yields the correct result", {
+	
+})
+
+test_that("d) yields the correct result", {
+	
+})
+
+test_that("e) yields the correct result", {
+	
+})
