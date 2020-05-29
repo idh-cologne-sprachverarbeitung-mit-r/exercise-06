f1f6951296f2b9e00bab731b2e3445821707e1c6
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-05-29 13:41:08 +0200

    reference solution

---
+++ R/exercise.R
@@ -0,0 +1,27 @@
+a <- function(n,v) {
+	n**(length(v)*mean(v))
+}
+
+juliet <- scan("data/tokenized-juliet.txt", sep="\n", what=character(), quiet=TRUE)
+b <- juliet[5]
+
+c <- c(mean(nchar(juliet)),sd(nchar(juliet)))
+
+
+# d)
+romeo <- scan("data/tokenized-romeo.txt", sep="\n", what=character(), quiet=TRUE)
+ttr <- function(x) { length(levels(factor(x)))/length(x) }
+
+juliet_tokens <- unlist(strsplit(juliet," "))
+romeo_tokens <- unlist(strsplit(romeo," "))
+
+d <- data.frame(
+	name=c("Juliet","Romeo"),
+	numSentences=c(length(juliet),length(romeo)),
+	numWords=c(length(juliet_tokens), length(romeo_tokens)),
+	ttr=c(ttr(juliet_tokens), ttr(romeo_tokens))
+)
+
+
+df <- read.csv("data/romeo-and-juliet.csv")
+e <- df[order(-df$numOfWords),"name"][1]
