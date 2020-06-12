5da376bdd5c14cfb95a2cb4ffe21cbac9858cf63
ariannabienati <abienati@smail.uni-koeln.de>
2020-06-12 08:45:41 +0200

    sorry wrong file

---
+++ R/exercise.R
@@ -0,0 +1,57 @@
+#a
+
+a <- function(numero, vettore) {
+  potenza <- (length(vettore)*mean(vettore))
+  risultato.numero <- numero^potenza
+  return(risultato.numero)
+}
+
+#b
+
+setwd("~/ariannabienati/exercise01/exercise-06")
+readLines("data/tokenized-juliet.txt")
+Juliet <- readLines("data/tokenized-juliet.txt")
+b <- Juliet[5]
+
+#c
+
+value.inter <- nchar(Juliet)
+value1 <- mean(value.inter)
+value2 <- sd(value.inter)
+c <- c(value1, value2)
+
+#d
+
+setwd("~/ariannabienati/exercise01/exercise-06")
+Juliet <- readLines("data/tokenized-juliet.txt")
+Romeo <- readLines("data/tokenized-romeo.txt")
+
+Juliet.split <- unlist(strsplit(Juliet, split=" "))
+Romeo.split <- unlist(strsplit(Romeo, split=" "))
+
+ttr <- function(x) { length(levels(factor(x)))/length(x) }
+Juliet.ttr <- ttr(Juliet.split)
+Romeo.ttr <- ttr(Romeo.split)
+
+Juliet.num.words <- length(Juliet.split)
+Romeo.num.words <- length(Romeo.split)
+
+Juliet.num.sentences <- length(Juliet)
+Romeo.length.sentences <- length(Romeo)
+
+Juliet.name <- "Juliet"
+Romeo.name <- "Romeo"
+
+Juliet.vec <- c(Juliet.name, Juliet.num.sentences, Juliet.num.words, Juliet.ttr)
+Romeo.vec <- c(Romeo.name, Romeo.length.sentences, Romeo.num.words, Romeo.ttr)
+
+d <- data.frame(rbind(Juliet.vec, Romeo.vec))
+
+names(d) <- c("name", "numSentences", "numWords", "ttr")
+
+#e
+
+setwd("~/ariannabienati/exercise01/exercise-06")
+last.df <- read.csv("data/romeo-and-juliet.csv")
+last.df[order(last.df$numOfWords, decreasing = TRUE) ,] -> ordered.last.df
+e <- ordered.last.df$name[1]
